cf6bde1f7ea6aa1b6358bb1b40ab85e628db8a88
Rupali <rupamargale15@gmail.com>
2021-05-13 10:57:46 +0530

    Static Block Program

---
+++ Fibonancci.java
@@ -0,0 +1,23 @@
+//Program for Fibonancci series
+import java.util.*;
+public class Fibonancci
+{
+   public static void main(String args[])
+   {
+     int k,a=1,b=1;
+	 Scanner sc=new Scanner(System.in);
+	 k=0;
+	 System.out.println("Enter any number");
+	 int n=sc.nextInt();
+	 System.out.print(" 1 1");
+	 while(k<=n)
+	 {
+	   k=a+b;
+	   if(k>=n)
+	   break;
+	   System.out.print(k+"  ");
+	   a=b;
+	   b=k;
+	   }
+	   }
+	   }
