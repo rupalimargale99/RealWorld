cdfba7e723fc11da203b1365c0e038e84c2194f4
Rupali <rupamargale15@gmail.com>
2021-05-13 10:52:10 +0530

    Static Block Program

---
+++ DemoStatic.java
@@ -0,0 +1,15 @@
+public class DemoStatic
+{
+  static
+  {
+    System.out.println("Hello World in static1");
+   }
+   public static void main(String args[])
+   {
+   System.out.println("In Main");
+   }
+   static
+   {
+   System.out.println("Bye in static2");
+   }
+   }
